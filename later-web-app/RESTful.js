aa77135fd729d51fe36e12a8f91eddb8326f0098
angelpray <1136472002@qq.com>
2018-12-25 22:31:08 +0800

    finish later project

---
+++ later-web-app/RESTful.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+const articles = [{ title: 'Example' }];
+
+app.get('/articles', (req, res) => {
+  res.send(articles);
+});
+
+app.post('/articles', (req, res) => {
+  res.send('OK');
+});
+
+app.get('/articles/:id', (req, res) => {
+  const {id} = req.params;
+  console.log('Fetching:', id);
+  res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res) => {
+  const {id} = req.params;
+  console.log('Deleting:', id);
+  delete articles[id];
+  res.send({ message: 'Deleted' });
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
